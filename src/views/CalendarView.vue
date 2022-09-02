22023440a27e21ade66a587b3d66fa17c5cbe7f5
Hwieun <dms7122@gmail.com>
2022-09-01 22:30:46 -0400

    change 12hr setting to 24hr setting

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -2,49 +2,48 @@
 import VueCal from "vue-cal";
 import "vue-cal/dist/vuecal.css";
 import axios from "axios";
+var moment = require("moment");
 
 export default {
   components: { VueCal },
   data: function () {
     return {
       events: [],
-      testEvents: [
-        {
-          id: 1,
-          title: "24w appt",
-          location: "8901 Rockville Pike",
-          memo: "Bring ID with Insurance card",
-          user_id: 1,
-          created_at: "2022-08-05T01:12:25.528Z",
-          updated_at: "2022-08-31T02:29:42.928Z",
-          start: "2022-09-01 12:00",
-          end: "2022-09-01 13:00",
-        },
-        {
-          id: 2,
-          title: "Baby shower",
-          location: "7400 Tuckerman Ln",
-          memo: "",
-          user_id: 1,
-          created_at: "2022-08-05T01:15:16.501Z",
-          updated_at: "2022-08-31T02:32:17.262Z",
-          start: "2022-09-17 11:00",
-          end: "2022-09-17 14:00",
-        },
-      ],
+      formattedEvents: [],
+      newEvent: {},
     };
   },
   created: function () {
     this.eventsIndex();
-    // this.fetchEvents();
   },
   methods: {
     eventsIndex: function () {
       axios.get("/events").then((response) => {
         console.log("events index", response);
         this.events = response.data;
+        for (let i = 0; i < response.data.length; i++) {
+          let event = {
+            title: response.data[i].title,
+            start: moment(response.data[i].start).format("YYYY-MM-DD H:mm"),
+            end: moment(response.data[i].end).format("YYYY-MM-DD H:mm"),
+          };
+          console.log(event);
+          this.formattedEvents.push(event);
+        }
       });
     },
+    // eventCreate: function () {
+    //   axios
+    //     .post("/events", this.newEvent)
+    //     .then((response) => {
+    //       console.log("event create", response);
+    //       this.$router.push("/events");
+    //     })
+    //     .catch((error) => {
+    //       console.log("event create error", error.response);
+    //       this.errors = error.response.data.errors;
+    //     });
+    // },
   },
 };
 </script>
@@ -53,19 +52,18 @@ export default {
   <div class="calendar">
     <vue-cal
       ref="vuecal"
-      :time-from="8 * 60"
+      :time-from="1 * 60"
       :time-to="23 * 60"
       style="height: 600px"
       :disable-views="['years', 'day']"
       active-view="month"
-      :events="testEvents"
+      :events="formattedEvents"
       events-on-month-view="short"
       :cell-click-hold="false"
       :drag-to-create-event="false"
       editable-events
       @cell-dblclick="$refs.vuecal.createEvent($event, 120, { title: 'New Event', class: 'blue-event' })"
     />
-    <!-- <div v-for="event in events" :key="event.id">{{ event }}</div> -->
   </div>
 </template>
 
