7140de197d3c059707ace572c2948c526f3c6efb
Hwieun <dms7122@gmail.com>
2022-09-05 20:53:00 -0400

    add css form to stayactive page

--- src/views/StayactiveView.vue
+++ src/views/StayactiveView.vue
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Stay Active",
+      message: "Stay Active: Quick Tips",
       sas: {
         title: "",
         content: "",
@@ -28,11 +28,52 @@ export default {
 </script>
 
 <template>
-  <div class="home">
-    <h1>{{ message }}</h1>
-    <h3>{{ sas.title }}</h3>
-    <div v-html="sas.content"></div>
+  <div class="stayactive">
+    <form action="/examples/actions/confirmation.php" method="post" class="form-horizontal">
+      <div class="row">
+        <div class="col-8 offset-4">
+          <h2>{{ message }}</h2>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div v-html="sas.content"></div>
+      </div>
+    </form>
   </div>
 </template>
 
-<style></style>
+<style>
+body {
+  color: #999;
+  background: #f3f3f3;
+  font-family: "Roboto", sans-serif;
+}
+.stayactive {
+  width: 1000px;
+  margin: 0 auto;
+  padding: 30px 0;
+}
+.stayactive h2 {
+  color: #333;
+  margin: 0 0 30px 0;
+  display: inline-block;
+  padding: 0 30px 10px 0;
+  border-bottom: 3px solid #ff6763;
+}
+.stayactive form {
+  color: #333;
+  border-radius: 3px;
+  margin-bottom: 15px;
+  background: #fff;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  padding: 30px;
+}
+.stayactive .form-group row {
+  margin-bottom: 20px;
+}
+.stayactive label {
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 2;
+}
+</style>
