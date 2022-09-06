fb4d175f0e775e0e5cd33a638575af80f939b36b
Hwieun <dms7122@gmail.com>
2022-09-05 20:48:03 -0400

    add css form to eathealthy page

--- src/views/EathealthyView.vue
+++ src/views/EathealthyView.vue
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Eat Healthy",
+      message: "Eat Healthy: Quick Tips",
       ehs: {
         title: "",
         content: "",
@@ -28,11 +28,57 @@ export default {
 </script>
 
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <h1>{{ message }}</h1>
     <h3>{{ ehs.title }}</h3>
     <div v-html="ehs.content"></div>
+  </div> -->
+  <div class="eathealthy">
+    <form action="/examples/actions/confirmation.php" method="post" class="form-horizontal">
+      <div class="row">
+        <div class="col-8 offset-4">
+          <h2>{{ message }}</h2>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div v-html="ehs.content"></div>
+      </div>
+    </form>
   </div>
 </template>
 
-<style></style>
+<style>
+body {
+  color: #999;
+  background: #f3f3f3;
+  font-family: "Roboto", sans-serif;
+}
+.eathealthy {
+  width: 1000px;
+  margin: 0 auto;
+  padding: 30px 0;
+}
+.eathealthy h2 {
+  color: #333;
+  margin: 0 0 30px 0;
+  display: inline-block;
+  padding: 0 30px 10px 0;
+  border-bottom: 3px solid #ff6763;
+}
+.eathealthy form {
+  color: #333;
+  border-radius: 3px;
+  margin-bottom: 15px;
+  background: #fff;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  padding: 30px;
+}
+.eathealthy .form-group row {
+  margin-bottom: 20px;
+}
+.eathealthy label {
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 2;
+}
+</style>
